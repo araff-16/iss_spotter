50a907a9a11652aa415288b1cfbaabf3cf505f16
araff-16 <alex_raffa16@hotmail.com>
2021-11-04 22:08:33 +0000

    added the find my coordinates feature

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+
 
 
 // fetchMyIP((error, ip) => {
@@ -9,3 +11,12 @@ const { fetchMyIP } = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+// fetchCoordsByIP('69.196.163.155', (error,data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('Coordinates: ', data)
+// })
--- iss.js
+++ iss.js
@@ -27,4 +27,31 @@ const fetchMyIP = function(callback) {
 };
 
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+
+  request(`https://freegeoip.app/json/${ip}`, (error,response,body)=>{
+    
+    if (error) {
+      callback(error, null);
+      return;
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    } else {
+      const latitude = JSON.parse(body).latitude;
+      const longitude = JSON.parse(body).longitude;
+      const data = {latitude: latitude, longitude: longitude};
+      
+      callback(error,data);
+    }
+
+  });
+
+
+
+};
+
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
